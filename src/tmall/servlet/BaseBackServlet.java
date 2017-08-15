f57202f450838ab89f704fa7d13b3c5493413c68
cwx2810 <cwx2810@163.com>
2017-08-15 09:23:35 +0800

    完成查询list

--- src/tmall/servlet/BaseBackServlet.java
+++ src/tmall/servlet/BaseBackServlet.java
@@ -6,7 +6,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tmall.dao.CategoryDAO;
+import tmall.dao.OrderDAO;
+import tmall.dao.OrderItemDAO;
+import tmall.dao.ProductDAO;
+import tmall.dao.ProductImageDAO;
+import tmall.dao.PropertyDAO;
+import tmall.dao.PropertyValueDAO;
+import tmall.dao.ReviewDAO;
+import tmall.dao.UserDAO;
+
 public abstract class BaseBackServlet extends HttpServlet{
+	
+	//声明DAO，具体的Servlet方法中会调用此父类的DAO对数据库进行操作
+	public abstract String add(HttpServletRequest request,HttpServletResponse response,Page page);
+	public abstract String delete(HttpServletRequest request,HttpServletResponse response,Page page);
+	public abstract String edit(HttpServletRequest request,HttpServletResponse response,Page page);
+	public abstract String update(HttpServletRequest request,HttpServletResponse response,Page page);
+	public abstract String list(HttpServletRequest request,HttpServletResponse response,Page page);
+	protected CategoryDAO categoryDAO = new CategoryDAO();
+	protected OrderDAO orderDAO = new OrderDAO();
+	protected OrderItemDAO orderItemDAO = new OrderItemDAO();
+	protected ProductDAO productDAO = new ProductDAO();
+	protected ProductImageDAO prodectImageDAO = new ProductImageDAO();
+	protected PropertyDAO propertyDAO = new PropertyDAO();
+	protected PropertyValueDAO propertyValueDAO = new PropertyValueDAO();
+	protected ReviewDAO reviewDAO = new ReviewDAO();
+	protected UserDAO userDAO = new UserDAO();
+	
 	//重写service方法，当调用具体Servlet类中的doGet、doPost方法时会到这里调用service
 	public void service(HttpServletRequest request,HttpServletResponse response){
 		try {
@@ -18,7 +45,13 @@ public abstract class BaseBackServlet extends HttpServlet{
 					javax.servlet.http.HttpServletResponse.class,page.class);
 			String redirect = m.invoke(this,request,response,page).toString();
 			
-			//根据方法返回值，进行跳转，或者输出字符串
+			//判断具体Servlet中方法返回值，进行服务端、客户端跳转（对跳转进行抽象，在方法中就不用每次麻烦的写），或者输出字符串
+			if(redirect.startsWith("@"))
+				response.sendRedirect(redirect.substring(1));
+			else if(redirect.startsWith("%"))
+				response.getWriter().print(redirect.substring(1));
+			else
+				request.getRequestDispatcher(redirect).forward(request, response);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/tmall/servlet/CategoryServlet.java
+++ src/tmall/servlet/CategoryServlet.java
@@ -1,5 +1,24 @@
 package tmall.servlet;
 
-public class CategoryServlet {
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tmall.bean.Category;
+
+public class CategoryServlet extends BaseBackServlet{
+	public String list(HttpServletRequest request,HttpServletResponse response,Page page){
+		//通过BaseServlet中声明DAO从数据库获取数据集合
+		List<Category> cs = categoryDAO.list(page.getStart(), page.getCount);
+		int total = categoryDAO.getTotal();
+		page.setTotal(total);
+		
+		//将取到的数据集合放在key中，在跳转到jsp后使用
+		request.setAttribute("thecs", cs);
+		request.setAttribute("page", page);
+		
+		//跳转到list的jsp，跳转判断已抽象在BaseServlet中
+		return "admin/listCategory.jsp";
+	}
 }
