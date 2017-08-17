caecac4f1132500509fe9c970c44fca2ebdfe120
cwx2810 <cwx2810@163.com>
2017-08-17 08:25:25 +0800

    添加filter解决中文问题

---
+++ src/tmall/filter/EncodingFilter.java
@@ -0,0 +1,28 @@
+package tmall.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EncodingFilter implements Filter{
+	public void destroy(){
+		
+	}
+	public void doFilter(ServletRequest req,ServletResponse res,FilterChain chain) throws IOException,ServletException{
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		//对提交的数据进行UTF8编码，别忘了在web.xml中设置配合的过滤器映射到这里
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+	public void init(FilterConfig arg0) throws ServletException{
+		
+	}
+}
